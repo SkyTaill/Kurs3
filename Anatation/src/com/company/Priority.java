76539f36df0017f598f5cc1f12e420b1a3d92bf8
maximVorobeichik <vorobeichik2015@yandex.ru>
2021-02-15 20:21:12 +0300

    7 DZ annotations

---
+++ Anatation/src/com/company/Priority.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Priority {
+    static final int Priority_1 = 1;
+    static final int Priority_2 = 2;
+    static final int Priority_3 = 3;
+    static final int Priority_4 = 4;
+    static final int Priority_5 = 5;
+    static final int Priority_6 = 6;
+    static final int Priority_7 = 7;
+    static final int Priority_8 = 8;
+    static final int Priority_9 = 9;
+    static final int Priority_10 = 10;
+}
